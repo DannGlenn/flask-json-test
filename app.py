0fd8053fb72107b4311295c198408af46f5751db
Daniel <110158094+DannGlenn@users.noreply.github.com>
2022-11-17 12:53:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def init():
+    return render_template('index.html')
 
 @app.route('/home',methods = ['POST', 'GET'])
 def hello_world():
